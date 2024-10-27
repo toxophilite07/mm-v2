635bac588eefcd03e0b9697bed3617b4bb4bd20b
Jee Ann Apitong <toxophilite07@gmail.com>
2024-10-28 05:19:48 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -233,23 +233,7 @@
 </div>
 
 
-    <div class="modal fade" id="newFeminineModal" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="newFeminineLbl" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="newFeminineLbl">Register New Feminine</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="col-md-12">
-                        @include('forms.add_new_form')
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
 
 @endsection
 
