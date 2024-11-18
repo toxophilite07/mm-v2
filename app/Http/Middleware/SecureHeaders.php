4cf75a40c5782cbb9b444af875e8cd28db0a7296
Jee Ann Apitong <toxophilite07@gmail.com>
2024-11-18 16:30:08 +0800

    Update SecureHeaders.php

--- app/Http/Middleware/SecureHeaders.php
+++ app/Http/Middleware/SecureHeaders.php
@@ -18,11 +18,14 @@ class SecureHeaders
         $response = $next($request);
 
         if (app()->environment('production')) {
-            $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self'; object-src 'none'; base-uri 'self';");
+            $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' https://example-cdn.com; object-src 'none'; base-uri 'self';");
             $response->headers->set('X-Frame-Options', 'DENY');
-            $response->headers->set('X-XSS-Protection', '1; mode=block');
             $response->headers->set('X-Content-Type-Options', 'nosniff');
             $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+            $response->headers->set('Referrer-Policy', 'no-referrer-when-downgrade');
+            $response->headers->set('Permissions-Policy', "geolocation=(), microphone=(), camera=()");
+            $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
+            $response->headers->set('Pragma', 'no-cache');
         }
 
         return $response;
