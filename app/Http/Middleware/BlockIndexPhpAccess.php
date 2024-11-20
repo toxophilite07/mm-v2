9387fa30d533e07a071ae3d1a822bcf8fdee41b7
Jee Ann Apitong <toxophilite07@gmail.com>
2024-11-21 07:27:15 +0800

    Update BlockIndexPhpAccess.php

--- app/Http/Middleware/BlockIndexPhpAccess.php
+++ app/Http/Middleware/BlockIndexPhpAccess.php
@@ -1,18 +1,29 @@
 <?php
-// namespace App\Http\Middleware;
 
-// use Closure;
+namespace App\Http\Middleware;
 
-// class BlockIndexPhpAccess
-// {
-//     public function handle($request, Closure $next)
-//     {
-//         // Check if the request contains 'index.php'
-//         if (strpos($request->getRequestUri(), 'index.php') !== false) {
-//             // Redirect to the same route without 'index.php'
-//             return redirect()->to(str_replace('index.php', '', $request->getRequestUri()));
-//         }
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-//         return $next($request);
-//     }
-// }
+class BlockIndexPhpAccess
+{
+    public function handle($request, Closure $next)
+    {
+        $uri = $request->getRequestUri();
+        
+        // Log the requested URI for debugging
+        Log::info("Requested URI: " . $uri);
+
+        // Check if the request contains 'index.php'
+        if (strpos($uri, 'index.php') !== false) {
+            // Log that we're redirecting
+            Log::info("Redirecting from: " . $uri);
+
+            // Redirect to the same route without 'index.php'
+            return redirect()->to(str_replace('index.php', '', $uri));
+        }
+
+        return $next($request);
+    }
+}
