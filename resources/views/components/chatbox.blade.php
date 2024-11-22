2b9606b01a0740beab9461717bc2dcc3ab3678e3
Jee Ann Apitong <toxophilite07@gmail.com>
2024-11-23 06:33:09 +0800

    Update chatbox.blade.php

--- resources/views/components/chatbox.blade.php
+++ resources/views/components/chatbox.blade.php
@@ -15,7 +15,7 @@
 </head>
 <body>
     <button class="floating-icon" onclick="toggleChatbox()">
-      <i class="fa-solid fa-question-circle fa-spin"></i> <!-- Spinning -->
+      <i class="fa-solid fa-question-circle fa-pulse"></i> <!-- Spinning -->
     </button>
 
     <div class="chatbox" id="chatbox">
