2e6dd25a54a850cc150b710dc40781f6f8654e66
Jee Ann Apitong <toxophilite07@gmail.com>
2024-12-04 14:19:45 +0800

    Update SecureHeaders.php
    Signed-off-by: Jee Ann Apitong <toxophilite07@gmail.com>

--- app/Http/Middleware/SecureHeaders.php
+++ app/Http/Middleware/SecureHeaders.php
@@ -18,7 +18,7 @@ class SecureHeaders
         $response = $next($request);
     
         if (app()->environment('production') && $response instanceof Response) {
-            $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' https://example-cdn.com; object-src 'none'; base-uri 'self';");
+            $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' base-uri 'self';");
             $response->headers->set('X-Frame-Options', 'DENY');
             $response->headers->set('X-Content-Type-Options', 'nosniff');
             $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
