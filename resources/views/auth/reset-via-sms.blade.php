de7db8842d1f80825f99899d95a6b5b062372818
Jee Ann Apitong <toxophilite07@gmail.com>
2024-12-08 16:26:19 +0800

    Update reset-via-sms.blade.php
    Signed-off-by: Jee Ann Apitong <toxophilite07@gmail.com>

--- resources/views/auth/reset-via-sms.blade.php
+++ resources/views/auth/reset-via-sms.blade.php
@@ -6,9 +6,9 @@
     <title>Mentrual Monitoring App :: Forgot Password via SMS::</title>
     <link rel="shortcut icon" type="image/png" href="{{ asset('assets/images/blood.jpg') }}" />
     <link rel="stylesheet" href="{{ asset('assets/auth/css/styles.min.css') }}" />
-
     <link rel="stylesheet" href="{{ asset('assets/izitoast/iziToast.min.css') }}">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+                                
     <style>
         .form-control { border-radius: 2px !important; }
         .btn-primary {
@@ -74,7 +74,7 @@
                                             <i class="fa-solid fa-paper-plane"></i> Send Password Reset Link
                                         </button>
                                         <a href="{{ route('forgot-password-options') }}" title="Back to Options" class="btn btn-light py-2 fs-5 w-100 rounded-1">
-                                            <i class="fa-solid fa-paper-plane"></i> Back to Options
+                                        <i class="fa-solid fa fa-arrow-left"></i> Back to Options
                                         </a>
                                     </div>
                                 </form>
