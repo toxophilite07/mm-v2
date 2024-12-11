a45c9c6e4de05a2f5db9bb6f04925b546f4951f6
Jee Ann Apitong <toxophilite07@gmail.com>
2024-12-11 08:40:22 +0800

    Create password_changed.blade.php
    Signed-off-by: Jee Ann Apitong <toxophilite07@gmail.com>

---
+++ resources/views/emails/password_changed.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Password Changed Successfully</title>
+</head>
+<body>
+    <h1>Password Changed Notification</h1>
+    <p>Hello,</p>
+    <p>Your account password has been successfully changed. If you did not perform this action, please contact our support team immediately.</p>
+    <p>Thank you,</p>
+    <p>The {{ config('app.name') }} Team</p>
+</body>
+</html>
