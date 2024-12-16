8f1bc69e777877224d64d6f1c88349eea1f631ad
Jee Ann Apitong <toxophilite07@gmail.com>
2024-12-16 13:12:26 +0800

    Update app.blade.php
    Signed-off-by: Jee Ann Apitong <toxophilite07@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,7 +87,7 @@
             <footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between">
                 <p class="text-muted text-center text-md-left">Copyright © {{ date('Y') }} <a href="mailto:nelbanbetache@gmail.com" target="_self">MCC Dev Team</a>. All rights reserved</p>
                 <p class="text-muted text-center text-md-left mb-0 d-none d-md-block">
-                    Powered by: <a href="https://www.facebook.com/nelban.betache.1">MCC Dev Team</a>
+                    Powered by: <a href="mailto:nelbanbetache@gmail.com">MCC Dev Team</a>
                 </p>
             </footer>
         </div>
