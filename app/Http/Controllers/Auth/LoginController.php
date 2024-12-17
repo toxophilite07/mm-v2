a0d69bc706a2f74a7625c5a3d0922c931e49f4fa
Jee Ann Apitong <toxophilite07@gmail.com>
2024-12-17 10:24:05 +0800

    Update LoginController.php
    Signed-off-by: Jee Ann Apitong <toxophilite07@gmail.com>

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -48,11 +48,11 @@ class LoginController extends Controller
             return redirect()->route('login.page');
         }
        // Check hCaptcha response
-        $hCaptchaResponse = $request->input('h-captcha-response');
-        if (!$hCaptchaResponse || !$this->verifyHCaptcha($hCaptchaResponse)) {
-            Session::flash('captcha-error', "Please verify that you are not a robot.");
-            return redirect()->route('login.page');
-        }
+       // $hCaptchaResponse = $request->input('h-captcha-response');
+       // if (!$hCaptchaResponse || !$this->verifyHCaptcha($hCaptchaResponse)) {
+       //     Session::flash('captcha-error', "Please verify that you are not a robot.");
+      //      return redirect()->route('login.page');
+     //   }
 
         $credentials = $request->only('password');
         $multi_user_field = filter_var($request->input('email'), FILTER_VALIDATE_EMAIL) ? 'email' : 'contact_no';
